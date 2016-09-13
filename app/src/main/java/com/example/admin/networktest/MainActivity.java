4e04939a5e1db09d738628e75e2af43722a0b469
mengzicheng <724686158@qq.com>
2016-09-13 22:01:37 +0800

    学习使用HttpClient

--- app/src/main/java/com/example/admin/networktest/MainActivity.java
+++ app/src/main/java/com/example/admin/networktest/MainActivity.java
@@ -8,17 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int SHOW_RESPONSE = 0;
     private Button sendRequest;
+    private Button httpclientsendRequest;
     private TextView responseText;
     private Handler handler = new Handler(){
         public void handleMessage(Message msg)
@@ -38,8 +49,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sendRequest = (Button) findViewById(R.id.send_request);
+        httpclientsendRequest = (Button) findViewById(R.id.httpclient_send_request);
         responseText = (TextView) findViewById(R.id.response_text);
+
         sendRequest.setOnClickListener(this);
+        httpclientsendRequest.setOnClickListener(this);
     }
 
     @Override
@@ -48,6 +62,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             sendRequestWithHttpURLConnection();
         }
+        if (v.getId() == R.id.httpclient_send_request)
+        {
+            sendRequestWithHttpClient();
+        }
+    }
+
+    private void sendRequestWithHttpClient() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if(httpResponse.getStatusLine().getStatusCode() == 200)
+                    {
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity, "utf-8");
+                        Message message = new Message();
+                        message.what =SHOW_RESPONSE;
+                        //将服务器返回的结果存放在Message中
+                        message.obj = response.toString();
+                        handler.sendMessage(message);
+                    }
+
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void sendRequestWithHttpURLConnection() {
